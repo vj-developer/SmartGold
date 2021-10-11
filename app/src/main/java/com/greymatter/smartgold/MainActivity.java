60d3a1a9bacb0e07811565bb3bae82f1c35e324f
madhu-minion <madhuminion20@gmail.com>
2021-10-11 23:21:45 +0530

    splash and fragment - completed

--- app/src/main/java/com/greymatter/smartgold/MainActivity.java
+++ app/src/main/java/com/greymatter/smartgold/MainActivity.java
@@ -1,14 +1,49 @@
 package com.greymatter.smartgold;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
+    BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(this);
+        bottomNavigationView.setSelectedItemId(R.id.home);
+
+    }
+    HomeFragment homeFragment = new HomeFragment();
+    SmartgoldFragment smartgoldFragment = new SmartgoldFragment();
+    CartFragment cartFragment = new CartFragment();
+    ProfileFragment profileFragment = new ProfileFragment();
+
+
+    public boolean onNavigationItemSelected(@NonNull MenuItem item){
+
+        switch (item.getItemId()){
+            case R.id.home:
+                getSupportFragmentManager().beginTransaction().replace(R.id.container,homeFragment).commit();
+                return true;
+
+            case R.id.smartgold:
+                getSupportFragmentManager().beginTransaction().replace(R.id.container,smartgoldFragment).commit();
+                return true;
+
+            case R.id.cart:
+                getSupportFragmentManager().beginTransaction().replace(R.id.container,cartFragment).commit();
+                return true;
+
+            case R.id.profile:
+                getSupportFragmentManager().beginTransaction().replace(R.id.container,profileFragment).commit();
+                return true;
+        }
+        return false;
     }
 }
---
+++ app/src/main/java/com/greymatter/smartgold/SmartgoldFragment.java
@@ -0,0 +1,23 @@
+package com.greymatter.smartgold;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class SmartgoldFragment extends Fragment {
+
+
+    public SmartgoldFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_smartgold, container, false);
+    }
+}
